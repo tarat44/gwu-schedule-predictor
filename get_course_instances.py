68712fbba366ea4660b2bd980262b46a71748cff
Tara <32471142+tarat44@users.noreply.github.com>
2019-04-27 11:28:53 -0400

    refactored the gathering of course data into its own module

--- get_course_instances.py
+++ get_course_instances.py
@@ -1,9 +1,7 @@
 import lxml.html
 import requests
-import argparse
 
 
-course_dict = {}
 class Course:
 
     def __init__(self, title, semester, professor, status): 
@@ -24,85 +22,68 @@ class Course:
         self.instances.append((semester, professor, status))
 
 
-def call_url_and_get_html_object(url):
-    resp = requests.get(url)
-    return lxml.html.fromstring(resp.text)
+class CollectCourseData:
 
+    def __init__(self):
+        self.course_dict = {}
 
-def get_semester_course_data(url, semester):
-    """inputs, url (str) to call, semester (str) that url find data for"""
-    print(f"Obtaining and indexing information for {semester}")
-    html = call_url_and_get_html_object(url)
-    tables = html.findall(".//table[@class='courseListing']")
+    def call_url_and_get_html_object(self, url):
+        resp = requests.get(url)
+        return lxml.html.fromstring(resp.text)
 
-    # Parse html to get course offering data
-    for table in tables:
-        fields = table.findall(".//td")
-        spans = table.findall(".//span")
-        course_number =  str(spans[1].text.strip())
-        title = str(fields[4].text).strip()
-        professor = str(fields[6].text).strip()
-        status = str(fields[0].text)
+    def get_semester_course_data(self, url, semester):
+        """inputs, url (str) to call, semester (str) that url find data for"""
+        print(f"Obtaining and indexing information for {semester}")
+        html = self.call_url_and_get_html_object(url)
+        tables = html.findall(".//table[@class='courseListing']")
 
-        # Add course offering data to dictionary of course classes
-        if course_number not in course_dict.keys():
-        # If course doesn't already exist in dictionary keys, instantiate class of it
-            course_dict[course_number] = Course(
-                title=title, semester=semester,
-                professor=professor, status=status)
-        else:
-            course_dict[course_number].add_instance_of_course(
-                semester, professor, status)
+        # Parse html to get course offering data
+        for table in tables:
+            fields = table.findall(".//td")
+            spans = table.findall(".//span")
+            course_number =  str(spans[1].text.strip())
+            title = str(fields[4].text).strip()
+            professor = str(fields[6].text).strip()
+            status = str(fields[0].text)
 
-def parse_args():
-    """Obtain subject and course number arguments from user and parse data"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--subject', type=str, dest='subject',
-                        help='The abbreviated subject of the course, ex: csci, bisc, chem')
-    parser.add_argument('--coursenum', type=str, dest='coursenum',
-                        help='Number of the course, ex: 1011, 1112')
-    args = parser.parse_args()
-    return args
+            # Add course offering data to dictionary of course classes
+            if course_number not in self.course_dict.keys():
+            # If course doesn't already exist in dictionary keys, instantiate class of it
+                self.course_dict[course_number] = Course(
+                    title=title, semester=semester,
+                    professor=professor, status=status)
+            else:
+                self.course_dict[course_number].add_instance_of_course(
+                    semester, professor, status)
 
+    def get_urls_from_term_strings(self, terms, subject):
+        """Inputs are a list of term html objects and a subject (string),
+           Returns a list of tuples containing the url (string) to obtain courses
+           in a given subject for each term and the term (string)"""
+        urls = []
+        for term in terms:
+            term = str(term.text).lower().strip()
+            term_split = term.split(" ")
+            if term_split[0] == "fall":
+                term_num = "03"
+            elif term_split[0] == "summer":
+                term_num= "02"
+            elif term_split[0] == "spring":
+                term_num = "01"
+            else:
+                raise Exception("Isssue parsing urls")
+            urls.append((f"https://my.gwu.edu/mod/pws/print.cfm?campId=1&termId={term_split[1]}{term_num}&subjId={subject}", term))
+        return urls
 
-def get_urls_from_term_strings(terms, subject):
-    """Inputs are a list of term html objects and a subject (string),
-       Returns a list of tuples containing the url (string) to obtain courses 
-       in a given subject for each term and the term (string)"""
-    urls = []
-    for term in terms:
-        term = str(term.text).lower().strip()
-        term_split = term.split(" ")
-        if term_split[0] == "fall":
-            term_num = "03"
-        elif term_split[0] == "summer":
-            term_num= "02"
-        elif term_split[0] == "spring":
-            term_num = "01"
-        else:
-            raise Exception("Isssue parsing urls")
-        urls.append((f"https://my.gwu.edu/mod/pws/print.cfm?campId=1&termId={term_split[1]}{term_num}&subjId={subject}", term))
-    return urls
+    def get_data_urls_and_terms(self, subject):
+        """Return list of urls to obtain data from available previous semesers"""
+        print("Finding terms with course information available")
+        html = self.call_url_and_get_html_object("https://my.gwu.edu/mod/pws/") # Call home page url
+        terms = html.findall(".//div[@class='tableHeaderFont'].//b") # Obtain list of available terms
+        return self.get_urls_from_term_strings(terms, subject)
 
-
-def get_data_urls_and_terms(subject):
-    """Return list of urls to obtain data from available previous semesers"""
-    print("Finding terms with course information available")
-    html = call_url_and_get_html_object("https://my.gwu.edu/mod/pws/") # Call home page url
-    terms = html.findall(".//div[@class='tableHeaderFont'].//b") # Obtain list of available terms
-    return get_urls_from_term_strings(terms, subject)
-
-def main():
-    args = parse_args()
-    subject = args.subject.upper()
-    urls = get_data_urls_and_terms(subject)
-    for url, semester in urls:
-        get_semester_course_data(url, semester)
-    coursenum = args.coursenum
-    try:
-        print(course_dict[coursenum].instances)
-    except KeyError:
-        print(f"The course {subject} {coursenum} has not been offered in the last two years")
-
-if __name__=="__main__":
-        main()
+    def collect_course_data(self, subject):
+        urls = self.get_data_urls_and_terms(subject)
+        for url, semester in urls:
+            self.get_semester_course_data(url, semester)
+        return self.course_dict
