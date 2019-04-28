bc2b095fb9536b8a62a655c88b1c4512f7bb0507
Tara <32471142+tarat44@users.noreply.github.com>
2019-04-28 00:43:38 -0400

    fixed method for detecting if class was offered in the last year

--- predict.py
+++ predict.py
@@ -7,7 +7,8 @@ class Predictor:
         self.course = course
         self.terms = terms
         self.semester = semester
-        self.latest_term = max(terms)
+        terms.sort()
+        self.latest_terms = terms[-3:]
 
     def predict(self):
         self.factor_in_enrollment()
@@ -16,13 +17,14 @@ class Predictor:
         self.create_percentage_from_score()
 
     def factor_in_enrollment(self):
+        recent = True
         for crn in self.course.instances.keys():
             if self.course.instances[crn][2].lower() == "open":
-                weight = 3
+                weight = 2
             elif self.course.instances[crn][2].lower() == "cancelled":
-                weight = -4
+                weight = -1
             elif self.course.instances[crn][2].lower() == "closed" or self.course.instances[crn][2].lower() == "waitlist":
-                weight = 4
+                weight = 3
             if self.within_last_year(self.course.instances[crn]):
                 weight = weight * 2
             self.score = self.score + weight
@@ -33,22 +35,22 @@ class Predictor:
             return self.course.semesters[0]
 
     def within_last_year(self, instance):
-        if int(translate_term_to_numerical(instance[0])[-2]) == self.latest_term:
+        if int(translate_term_to_numerical(instance[0])) in self.latest_terms:
                 return True
 
     def factor_in_multiple_professors(self):
         prev_profs = [self.course.instances[list(self.course.instances.keys())[0]][1]]
         for crn in self.course.instances.keys():
             if self.course.instances[crn][1] not in prev_profs and self.course.instances[crn][1].lower() != "none":
-                self.score = self.score + 2
-            prev_profs.append(self.course.instances[crn][1])
+                self.score = self.score + 1
+                prev_profs.append(self.course.instances[crn][1])
 
     def factor_in_semester_offered(self):
         if self.is_seasonal_course() and self.is_seasonal_course().split(" ")[0].lower() != self.semester.lower():
             self.score = self.score - 20
 
     def create_percentage_from_score(self):
-        self.score = self.score * 30
+        self.score = self.score * 10
         if self.score >= 100:
             self.score = 99
         if self.score <= 0:
