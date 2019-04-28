a84e67012c62899a9202a804add05afea4d73df6
Tara <32471142+tarat44@users.noreply.github.com>
2019-04-27 23:02:21 -0400

    algorithm updates

--- predict.py
+++ predict.py
@@ -7,10 +7,10 @@ class Predictor:
         self.course = course
         self.terms = terms
         self.semester = semester
+        self.latest_term = max(terms)
 
     def predict(self):
         self.factor_in_enrollment()
-        self.factor_in_latest_term_available()
         self.factor_in_multiple_professors()
         self.factor_in_semester_offered()
         self.create_percentage_from_score()
@@ -18,47 +18,37 @@ class Predictor:
     def factor_in_enrollment(self):
         for crn in self.course.instances.keys():
             if self.course.instances[crn][2].lower() == "open":
-                self.score = self.score + 3
+                weight = 3
             elif self.course.instances[crn][2].lower() == "cancelled":
-                self.score = self.score - 4
+                weight = -4
             elif self.course.instances[crn][2].lower() == "closed" or self.course.instances[crn][2].lower() == "waitlist":
-                self.score = self.score + 4
+                weight = 4
+            if self.within_last_year(self.course.instances[crn]):
+                weight = weight * 2
+            self.score = self.score + weight
 
     def is_seasonal_course(self):
         seasons = [semester.split(" ")[0] for semester in self.course.semesters]
         if len(set(seasons)) == 1:
             return self.course.semesters[0]
 
-    def get_latest_season_term(self, season):
-        seasons = []
-        for term in self.terms:
-            if str(term)[-2] == season:
-                seasons.append(term)
-        return max(seasons)
-
-    def factor_in_latest_term_available(self):
-        if self.is_seasonal_course():
-            season = translate_term_to_numerical(self.is_seasonal_course())[-2]
-            latest_term = self.get_latest_season_term(season)
-        else:
-            latest_term = max(self.terms)
-        for crn in self.course.instances.keys():
-            if int(translate_term_to_numerical(self.course.instances[crn][0])) == latest_term:
-                self.score = self.score + 5
+    def within_last_year(self, instance):
+        if int(translate_term_to_numerical(instance[0])[-2]) == self.latest_term:
+                return True
 
     def factor_in_multiple_professors(self):
         prev_profs = [self.course.instances[list(self.course.instances.keys())[0]][1]]
         for crn in self.course.instances.keys():
             if self.course.instances[crn][1] not in prev_profs and self.course.instances[crn][1].lower() != "none":
-                self.score = self.score + 1
+                self.score = self.score + 2
             prev_profs.append(self.course.instances[crn][1])
 
     def factor_in_semester_offered(self):
         if self.is_seasonal_course() and self.is_seasonal_course().split(" ")[0].lower() != self.semester.lower():
-            self.score = self.score - 15
+            self.score = self.score - 20
 
     def create_percentage_from_score(self):
-        self.score = self.score * 9
+        self.score = self.score * 30
         if self.score >= 100:
             self.score = 99
         if self.score <= 0:
