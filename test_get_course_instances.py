e5827cc390ecfb926ad13219982f1fc9aa5f4d8f
Tara <32471142+tarat44@users.noreply.github.com>
2019-09-05 01:08:21 -0400

    Add tests, refine code

---
+++ test_get_course_instances.py
@@ -0,0 +1,15 @@
+from get_course_instances import CollectCourseData, Course
+
+def test_add_instance_of_course():
+    course = Course("Intro Bio", "Fall 2019", "Johnson", "1234", "OPEN")
+    course.add_instance_of_course("Spring 2020", "Smith", "5678", "CLOSED")
+
+    assert course.semesters == ["Fall 2019", "Spring 2020"]
+    assert course.statuses == ["OPEN", "CLOSED"]
+    assert course.professors == ["Johnson", "Smith"]
+    assert course.instances["1234"] == ("Fall 2019", "Johnson", "OPEN")
+    assert course.instances["5678"] == ("Spring 2020", "Smith", "CLOSED")
+
+def test_get_url_from_term_strings():
+    assert CollectCourseData().get_url_from_term_string(
+        "201802", "BISC") == "https://my.gwu.edu/mod/pws/print.cfm?campId=1&termId=201802&subjId=BISC"
--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
+term_dict = {
+    "fall": "03",
+    "summer": "02",
+    "spring": "01"
+}
+
 def translate_term_to_numerical(term):
     term_split = term.split(" ")
-    if term_split[0] == "fall":
-        term_num = "03"
-    elif term_split[0] == "summer":
-        term_num= "02"
-    elif term_split[0] == "spring":
-        term_num = "01"
-    else:
+    try:
+        return str(term_split[1] + term_dict[term_split[0]])
+    except KeyError:
         raise Exception("Error parsing term")
-    return str(term_split[1] + term_num)
