81c4915670c59a422fdfa26b43372ff63e86a334
Tara Tufano <32471142+tarat44@users.noreply.github.com>
2019-09-05 01:24:51 -0400

    Update get_course_instances.py

--- get_course_instances.py
+++ get_course_instances.py
@@ -59,16 +59,11 @@ class CollectCourseData:
                 self.course_dict[course_number].add_instance_of_course(
                     semester, professor, crn, status)
 
-    def get_urls_from_term_strings(self, terms, subject):
-        """Inputs are a list of term html objects and a subject (string),
+    def get_url_from_term_string(self, num_term, subject):
+        """Inputs are a term number (int) and a subject (string),
            Returns a list of tuples containing the url (string) to obtain courses
            in a given subject for each term and the term (string)"""
-        urls = []
-        for term in terms:
-            num_term = translate_term_to_numerical(term)
-            urls.append((f"https://my.gwu.edu/mod/pws/print.cfm?campId=1&termId={num_term}&subjId={subject}", term))
-            self.terms.append(int(num_term))
-        return urls
+        return f"https://my.gwu.edu/mod/pws/print.cfm?campId=1&termId={num_term}&subjId={subject}"
 
     def get_data_urls_and_terms(self, subject):
         """Return list of urls to obtain data from available previous semesers"""
@@ -76,7 +71,13 @@ class CollectCourseData:
         html = self.call_url_and_get_html_object("https://my.gwu.edu/mod/pws/") # Call home page url
         term_elements = html.findall(".//div[@class='tableHeaderFont'].//b") # Obtain list of available terms
         terms = [term.text.lower().strip() for term in term_elements]
-        return self.get_urls_from_term_strings(terms, subject), terms
+        term_urls = []
+
+        for term in terms:
+            num_term = translate_term_to_numerical(term)
+            self.terms.append(int(num_term))
+            term_urls.append((self.get_url_from_term_string(num_term, subject), term))
+        return term_urls
 
     def collect_course_data(self, subject):
         urls, terms = self.get_data_urls_and_terms(subject)
